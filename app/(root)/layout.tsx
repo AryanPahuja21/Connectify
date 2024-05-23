e17d80b8f629250a6de9d498f1022b2c8256a5d4
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-05-23 19:30:00 +0530

    meeting creation logic added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
 import React, { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
-export default layout;
+export default RootLayout;
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 
 import HomeCard from "./HomeCard";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const initialValues = {
   dateTime: new Date(),
@@ -18,9 +20,45 @@ const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
+  const user = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
 
-  const createMeeting = () => {
-    console.log("Meeting created");
+  const createMeeting = async () => {
+    if (!client || !user) return;
+
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date().toISOString();
+      const description = values.description || "Instant Meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${id}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
