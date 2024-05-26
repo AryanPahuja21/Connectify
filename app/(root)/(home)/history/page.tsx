3b53bab8160c594bee5731f0f6cd40d37a7ca924
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-05-26 17:19:27 +0530

    history and recordings mapped

--- app/(root)/(home)/history/page.tsx
+++ app/(root)/(home)/history/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import CallList from "@/components/CallList";
 
 const History = () => {
   return (
-    <div>
-      <h1>Meeting History</h1>
-    </div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Meeting History</h1>
+
+      <CallList type="ended" />
+    </section>
   );
 };
 
--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import CallList from "@/components/CallList";
 
 const Recordings = () => {
   return (
-    <div>
-      <h1>Meeting Recordings</h1>
-    </div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Recordings</h1>
+
+      <CallList type="recordings" />
+    </section>
   );
 };
 
